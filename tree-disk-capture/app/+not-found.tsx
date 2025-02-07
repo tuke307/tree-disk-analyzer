1c39d31307d2f45ff7a0929c6425dbc8d3783288
Tony <tonymeissner70@gmail.com>
2025-02-07 13:52:59 +0100

    refactor: simplify NotFoundScreen layout and remove unused styles

--- tree-disk-capture/app/+not-found.tsx
+++ tree-disk-capture/app/+not-found.tsx
@@ -1,31 +1,18 @@
 import { Label } from '@/components/ui/label';
 import { H1 } from '@/components/ui/typography';
 import { Link, Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <View style={styles.container}>
+      <View className="flex-1 items-center justify-center p-5">
         <H1>This screen doesn't exist.</H1>
-        <Link href="/" style={styles.link}>
+        <Link href="/" className="mt-4 py-4">
           <Label>Go to home screen!</Label>
         </Link>
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
