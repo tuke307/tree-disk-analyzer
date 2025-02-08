e51b8d6b2775948ab630d635b5c86ed417e33724
Tony <tonymeissner70@gmail.com>
2025-02-08 21:29:38 +0100

    feat: update NotFoundScreen to use Button component for navigation and improve layout

--- tree-disk-capture/app/+not-found.tsx
+++ tree-disk-capture/app/+not-found.tsx
@@ -1,17 +1,21 @@
-import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
 import { H1 } from '@/components/ui/typography';
-import { Link, Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { View } from 'react-native';
 
 export default function NotFoundScreen() {
+  const router = useRouter();
+
   return (
     <View className="flex-1">
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <View className="flex-1 items-center justify-center p-5">
+      <View className="flex-1 gap-4 items-center justify-center p-5">
         <H1>This screen doesn't exist.</H1>
-        <Link href="/" className="mt-4 py-4">
-          <Label>Go to home screen!</Label>
-        </Link>
+
+        <Button variant="outline" onPress={() => router.replace('/')}>
+          <Text>Go to home screen!</Text>
+        </Button>
       </View>
     </View>
   );
