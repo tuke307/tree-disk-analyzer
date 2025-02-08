f2798455f17ea693d7c69d2617821a37fbe6d676
Tony <tonymeissner70@gmail.com>
2025-02-08 21:35:53 +0100

    feat: add ArrowRightIcon component for enhanced icon usage

---
+++ tree-disk-capture/lib/icons/ArrowRightIcon.tsx
@@ -0,0 +1,4 @@
+import { ArrowRightIcon } from 'lucide-react-native';
+import { iconWithClassName } from '@/lib/icons/iconWithClassName';
+iconWithClassName(ArrowRightIcon);
+export { ArrowRightIcon };
