d1dd5d36f9550bb35785cb97d6b5ee035ae0fa09
Tony <tonymeissner70@gmail.com>
2025-02-13 00:30:24 +0100

    feat: update ImagePreview component layout and enhance button functionality

--- tree-disk-capture/components/camera/image-preview.tsx
+++ tree-disk-capture/components/camera/image-preview.tsx
@@ -18,33 +18,36 @@ export function ImagePreview({
 }: ImagePreviewProps) {
   return (
     <SafeAreaView className="flex-1">
-      <View className="flex-1">
+      <View className="flex-1 m-12">
         <Image
           source={{ uri: base64 }}
-          className='flex-1 m-32'
           contentFit="contain"
+          transition={1000}
+          // somehow tailwind is ignored here, so we need to use the style prop
+          style={{ flex: 1, width: '100%', height: '100%' }}
         />
+      </View>
 
-        <View className='flex-row justify-between p-8'>
-          <Button
-            onPress={onRetake}
-            className='flex-row gap-2 items-center'>
-            <RotateCcwIcon className='text-primary-foreground' />
-            <Text>
-              Retake
-            </Text>
-          </Button>
+      <View className='flex-row justify-between p-8'>
+        <Button
+          onPress={onRetake}
+          className='flex-row gap-2 items-center'>
+          <RotateCcwIcon className='text-primary-foreground' />
+          <Text>
+            Retake
+          </Text>
+        </Button>
 
-          <Button
-            onPress={onSave}
-            className='flex-row gap-2 items-center'
-          >
-            <ArrowRightIcon className='text-primary-foreground' />
-            <Text>
-              Next
-            </Text>
-          </Button>
-        </View>
+        <Button
+          onPress={onSave}
+          className='flex-row gap-2 items-center'
+        >
+          <ArrowRightIcon className='text-primary-foreground' />
+          <Text>
+            Next
+          </Text>
+        </Button>
+
       </View>
     </SafeAreaView>
   );
