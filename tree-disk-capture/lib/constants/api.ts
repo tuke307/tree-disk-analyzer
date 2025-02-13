b373d1954a3f0e64c051887a19a6cb0ada689fc9
Tony <tonymeissner70@gmail.com>
2025-02-13 23:53:59 +0100

    feat: update API_URL to use environment variable for better configuration

--- tree-disk-capture/lib/constants/api.ts
+++ tree-disk-capture/lib/constants/api.ts
@@ -1,4 +1,1 @@
-export const API_URL: string = 'https://api-treediskanalyzer-prod.azurewebsites.net';
-
-// const API_URL = 'http://127.0.0.1:8000';
-// const API_URL = 'https://api-treediskanalyzer-prod.azurewebsites.net';
+export const EXPO_PUBLIC_API_URL: string = process.env.EXPO_PUBLIC_API_URL || 'http://127.0.0.1:8000';
