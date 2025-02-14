28a60a94fa1b467efe743719a521c005d04753b5
Tony <tonymeissner70@gmail.com>
2025-02-14 21:36:32 +0100

    fix: return float type for euclidean_distance function result

--- tree-disk-pith/src/treediskpith/geometry/geometry_utils.py
+++ tree-disk-pith/src/treediskpith/geometry/geometry_utils.py
@@ -12,4 +12,4 @@ def euclidean_distance(p1: np.ndarray, p2: np.ndarray) -> float:
     Returns:
         The Euclidean distance between p1 and p2.
     """
-    return np.linalg.norm(p1 - p2)
+    return float(np.linalg.norm(p1 - p2))
