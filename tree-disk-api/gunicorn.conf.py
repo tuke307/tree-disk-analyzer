445be5b0b8752d382d5b1ea535a916c34937455d
Tony <tonymeissner70@gmail.com>
2025-02-14 20:30:18 +0100

    feat: update .env.sample to include Gunicorn settings

--- tree-disk-api/gunicorn.conf.py
+++ tree-disk-api/gunicorn.conf.py
@@ -1,8 +1,16 @@
 import multiprocessing
 import os
 
+# Basic config
 pythonpath = "src"
 bind = "0.0.0.0:3100"
 workers = multiprocessing.cpu_count()  # * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
-timeout = os.environ.get("GUNICORN_TIMEOUT", 120)
+
+# Timeout config
+timeout = int(os.getenv("GUNICORN_TIMEOUT", "120"))
+graceful_timeout = int(os.getenv("GUNICORN_GRACEFUL_TIMEOUT", "120"))
+keepalive = int(os.getenv("GUNICORN_KEEP_ALIVE", "5"))
+
+# Production settings
+preload_app = True  # Load application code before worker processes are forked
