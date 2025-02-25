3a602747c05bfb514c25d78edc75b39e049bc634
Tony <tonymeissner70@gmail.com>
2025-02-25 17:03:01 +0100

    feat: update YOLO detection function to return object masks and improve logging

--- tree-disk-segmentation/src/treedisksegmentation/models/yolo.py
+++ tree-disk-segmentation/src/treedisksegmentation/models/yolo.py
@@ -3,13 +3,14 @@ import numpy as np
 from ultralytics import YOLO
 from typing import Union, Optional, List, Dict
 from pathlib import Path
+from PIL import Image
 
 from ..config import config
 
 logger = logging.getLogger(__name__)
 
 
-def run_yolo_detection(img_in: np.ndarray) -> dict:
+def run_yolo_detection(img_in: np.ndarray) -> Optional[List[np.ndarray]]:
     """
     Uses a YOLO detection model to perform object detection on the input image.
 
@@ -19,44 +20,40 @@ def run_yolo_detection(img_in: np.ndarray) -> dict:
         model_path (str): Path to the trained model weights.
 
     Returns:
-        dict: Detection results in JSON format.
+        Optional[List[np.ndarray]]: The detected object masks as a list of polygons.
     """
     if config.model_path is None:
         raise ValueError("model_path is None")
 
+    # yolo needs the image as a PIL image
+    img_pil = Image.fromarray(img_in)
+
     model = YOLO(config.model_path, task="detect", verbose=config.debug)
 
     results = model(
-        img_in,
+        img_pil,
         project=config.output_dir,
         save=config.save_results,
         save_txt=config.save_results,
     )
 
-    logger.info("YOLO detection complete")
-    logger.info(f"Results: {results}")
+    results_len = len(results)
 
-    result_json = results[0].to_json() if hasattr(results[0], "to_json") else results[0]
-    if "predictions" not in result_json or not result_json["predictions"]:
-        raise ValueError("No predictions found in YOLO result")
+    logger.info(f"YOLO detection complete, {results_len} log(s) found.")
 
-    return result_json
+    if results_len == 0:
+        logger.error("No results found in YOLO detection.")
+        return None
 
+    # were just using the first result, so the first log found
+    first_result = results[0]
 
-def get_polygon_points(
-    detections: dict, class_name: str = "log"
-) -> Optional[List[Dict[str, float]]]:
-    """
-    Extracts polygon points from the detections for the specified class.
+    # xy = first_result.masks.xy  # mask in polygon format
+    # xyn = first_result.masks.xyn  # normalized
+    # masks = first_result.masks.data  # mask in matrix format (num_objects x H x W)
 
-    Args:
-        detections (dict): Detection results in JSON format.
-        class_name (str): The class name to filter predictions (default "log").
-
-    Returns:
-        Optional[List[Dict[str, float]]]: The list of polygon points if found, otherwise None.
-    """
-    for prediction in detections["predictions"]:
-        if prediction.get("class", "").lower() == class_name.lower():
-            return prediction.get("points", None)
-    return None
+    if first_result.masks is not None and first_result.masks.xy is not None:
+        return first_result.masks.xy
+    else:
+        logger.warning("No mask polygons found in the YOLO result.")
+        return None
