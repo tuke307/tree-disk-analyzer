001c5bdcbb6da29717bc6e040dc38c839888eed7
Tony <tonymeissner70@gmail.com>
2025-02-25 17:03:29 +0100

    refactor: update run function to return masks instead of polygon points and streamline image processing

--- tree-disk-segmentation/src/treedisksegmentation/main.py
+++ tree-disk-segmentation/src/treedisksegmentation/main.py
@@ -6,17 +6,16 @@ import numpy as np
 from PIL import Image
 
 from .config import config
-from .models.yolo import run_yolo_detection, get_polygon_points
+from .models.yolo import run_yolo_detection
 from .utils.file_utils import load_image, save_image
 from .segmentation.segmentation import (
-    polygon_to_mask,
     apply_mask,
 )
 
 logger = logging.getLogger(__name__)
 
 
-def run() -> Tuple[np.ndarray, Optional[List[Dict[str, float]]]]:
+def run() -> Tuple[np.ndarray, Optional[List[np.ndarray]]]:
     """
     Pipeline to remove the salient object while maintaining the original resolution.
 
@@ -32,35 +31,20 @@ def run() -> Tuple[np.ndarray, Optional[List[Dict[str, float]]]]:
         config.log_all_configs()
 
         logger.info(f"Loading input image: {config.input_image}")
-        img_in = load_image(config.input_image)
-        img_pil = Image.fromarray(img_in)
+        img_in = load_image(str(config.input_image))
 
-        logger.info("Running YOLO object detection...")
-        result_json = run_yolo_detection(img_in)
+        logger.info("Running YOLO object segmentation...")
+        masks = run_yolo_detection(img_in)
 
-        logger.info("Extracting polygon points...")
-        polygon_points = get_polygon_points(result_json)
-
-        if not polygon_points:
-            logger.error("No polygon points found in YOLO result.")
+        if not masks:
             return np.array([]), None
 
-        logger.info("Running salient object detection...")
-        mask = polygon_to_mask(img_pil, polygon_points)
-
         logger.info("Applying mask to original image...")
-        result_image, mask_uint8 = apply_mask(img_pil, mask)
-
-        if config.save_results:
-            output_path = Path(config.output_dir)
-            output_image_path = os.path.join(output_path, "output.jpg")
-
-            save_image(output_image_path, result_image)
-            logger.info(f"Result image saved at: {output_image_path}")
+        result_image = apply_mask(img_in, masks)
 
         logger.debug(f"Done.")
 
-        return result_image, polygon_points
+        return result_image, masks
 
     except Exception as e:
         logger.error(f"Error during processing: {str(e)}", exc_info=True)
