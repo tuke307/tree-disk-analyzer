c25c036b3b06bf79189e91f4fb9604db51503ff4
Tony <tonymeissner70@gmail.com>
2025-03-06 23:35:09 +0100

    fix: add text color to success and error icons in ProgressStep component

--- tree-disk-capture/components/history/progress-step.tsx
+++ tree-disk-capture/components/history/progress-step.tsx
@@ -24,7 +24,7 @@ export const ProgressStep = ({ label, loading, success }: ProgressStepProps) => 
         {loading ? (
           <ActivityIndicator />
         ) : (
-          success ? <CheckIcon /> : <XIcon />
+          success ? <CheckIcon className='text-foreground' /> : <XIcon className='text-foreground' />
         )}
       </Button>
       <Text>{label}</Text>
